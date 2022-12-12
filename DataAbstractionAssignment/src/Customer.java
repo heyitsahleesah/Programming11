d23fcacd9a8d3deaa36777a3c98141e543cc8479
Alissa <alissa.graham88@gmail.com>
2022-12-11 18:39:30 -0800

    Updated comments, constructors, deposit method

--- DataAbstractionAssignment/src/Customer.java
+++ DataAbstractionAssignment/src/Customer.java
@@ -1,6 +1,7 @@
 import javax.security.sasl.SaslClient;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Objects;
 
 public class Customer {
     private int accountNumber;
@@ -14,29 +15,51 @@ public class Customer {
     public static final String SAVING = "Saving";
     private final int OVERDRAFT = -100;
 
+    //Default
     Customer(){
-        name
+        this.accountNumber += 1;
+        this.name = "John Doe";
+        this.deposits = new ArrayList<Deposit>();
+        this.withdraws = new ArrayList<Withdraw>();
+        this.checkBalance = 0.0;
+        this.savingBalance = 0.0;
     }
 
     Customer(String name, int accountNumber, double checkDeposit, double savingDeposit){
-        deposits = new ArrayList<Deposit>;
-        withdrawn = new ArrayList<Withdraws>;
-        name = name;
-        accountNumber = accountNumber;
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.deposits = new ArrayList<Deposit>();
+        withdraws = new ArrayList<Withdraw>();
+        this.checkBalance += checkDeposit;
+        this.savingBalance += savingDeposit;
     }
 
+    //Requires: double, date, string
+    //Modifies: this
+    //Effects: Add the amount to the specified account (checking or savings).
     public double deposit(double amt, Date date, String account){
-        //your code here
+        if(Objects.equals(account, CHECKING)) {
+            checkBalance += amt;
+            System.out.println("Deposit of: " + amt + "Date: " + new Date() + "into account: " + account + "Current" +
+                    "balance in " + account + " is " + checkBalance);
+        }
+        System.out.println("Deposit of: " + amt + "Date: " + new Date() + "into account: " + account + "Current" +
+                "balance in " + account + " is " + savingBalance);
         return 0;
     }
+
+    //Requires: double, date, string
+    //Modifies: this
+    //Effects: Minuses the amount from the specified account.
     public double withdraw(double amt, Date date, String account){
         //your code here
         return 0;
     }
+
+    //Requires: double, string
+    //Effects
     private boolean checkOverdraft(double amt, String account){
-        if amount < OVERDRAFT {
-            return true;
-        }
+        //your code here
         return false;
     }
     //do not modify
